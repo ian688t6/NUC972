8406267ec539bfb40b1f32062bb06f6bfca3759a
ian688t6 <ian688t6@foxmail.com>
2019-04-01 17:00:30 +0800

    add busybox
    Change-Id: I4b3918b884715c89e4411ed8056534cd293d11e4

---
+++ opensrc/busybox/busybox-1.22.1/include/applet_tables.h
@@ -0,0 +1,270 @@
+/* This is a generated file, don't edit */
+
+#define NUM_APPLETS 85
+
+const char applet_names[] ALIGN1 = ""
+"ash" "\0"
+"bash" "\0"
+"cat" "\0"
+"catv" "\0"
+"chmod" "\0"
+"chown" "\0"
+"cp" "\0"
+"cpio" "\0"
+"cut" "\0"
+"date" "\0"
+"dd" "\0"
+"depmod" "\0"
+"df" "\0"
+"dnsdomainname" "\0"
+"dos2unix" "\0"
+"echo" "\0"
+"egrep" "\0"
+"env" "\0"
+"false" "\0"
+"fgrep" "\0"
+"getty" "\0"
+"grep" "\0"
+"gunzip" "\0"
+"gzip" "\0"
+"halt" "\0"
+"hostname" "\0"
+"hush" "\0"
+"ifconfig" "\0"
+"ifdown" "\0"
+"ifenslave" "\0"
+"ifplugd" "\0"
+"ifup" "\0"
+"init" "\0"
+"insmod" "\0"
+"ip" "\0"
+"ipaddr" "\0"
+"iplink" "\0"
+"iproute" "\0"
+"iprule" "\0"
+"iptunnel" "\0"
+"kill" "\0"
+"killall" "\0"
+"killall5" "\0"
+"linuxrc" "\0"
+"ln" "\0"
+"ls" "\0"
+"lsmod" "\0"
+"mdev" "\0"
+"mesg" "\0"
+"mkdir" "\0"
+"mknod" "\0"
+"modinfo" "\0"
+"modprobe" "\0"
+"mount" "\0"
+"mv" "\0"
+"netstat" "\0"
+"passwd" "\0"
+"ping" "\0"
+"poweroff" "\0"
+"printenv" "\0"
+"printf" "\0"
+"ps" "\0"
+"pwd" "\0"
+"reboot" "\0"
+"rm" "\0"
+"rmdir" "\0"
+"rmmod" "\0"
+"sed" "\0"
+"sh" "\0"
+"sleep" "\0"
+"stat" "\0"
+"sync" "\0"
+"tar" "\0"
+"top" "\0"
+"true" "\0"
+"udhcpc" "\0"
+"umount" "\0"
+"uncompress" "\0"
+"unix2dos" "\0"
+"usleep" "\0"
+"vi" "\0"
+"watch" "\0"
+"wc" "\0"
+"wget" "\0"
+"zcat" "\0"
+;
+
+#ifndef SKIP_applet_main
+int (*const applet_main[])(int argc, char **argv) = {
+ash_main,
+ash_main,
+cat_main,
+catv_main,
+chmod_main,
+chown_main,
+cp_main,
+cpio_main,
+cut_main,
+date_main,
+dd_main,
+modprobe_main,
+df_main,
+hostname_main,
+dos2unix_main,
+echo_main,
+grep_main,
+env_main,
+false_main,
+grep_main,
+getty_main,
+grep_main,
+gunzip_main,
+gzip_main,
+halt_main,
+hostname_main,
+hush_main,
+ifconfig_main,
+ifupdown_main,
+ifenslave_main,
+ifplugd_main,
+ifupdown_main,
+init_main,
+modprobe_main,
+ip_main,
+ipaddr_main,
+iplink_main,
+iproute_main,
+iprule_main,
+iptunnel_main,
+kill_main,
+kill_main,
+kill_main,
+init_main,
+ln_main,
+ls_main,
+modprobe_main,
+mdev_main,
+mesg_main,
+mkdir_main,
+mknod_main,
+modinfo_main,
+modprobe_main,
+mount_main,
+mv_main,
+netstat_main,
+passwd_main,
+ping_main,
+halt_main,
+printenv_main,
+printf_main,
+ps_main,
+pwd_main,
+halt_main,
+rm_main,
+rmdir_main,
+modprobe_main,
+sed_main,
+ash_main,
+sleep_main,
+stat_main,
+sync_main,
+tar_main,
+top_main,
+true_main,
+udhcpc_main,
+umount_main,
+uncompress_main,
+dos2unix_main,
+usleep_main,
+vi_main,
+watch_main,
+wc_main,
+wget_main,
+gunzip_main,
+};
+#endif
+
+const uint16_t applet_nameofs[] ALIGN2 = {
+0x0000,
+0x0004,
+0x0009,
+0x000d,
+0x0012,
+0x0018,
+0x001e,
+0x0021,
+0x0026,
+0x002a,
+0x002f,
+0x0032,
+0x0039,
+0x003c,
+0x004a,
+0x0053,
+0x0058,
+0x005e,
+0x0062,
+0x0068,
+0x006e,
+0x0074,
+0x0079,
+0x0080,
+0x0085,
+0x008a,
+0x0093,
+0x0098,
+0x00a1,
+0x00a8,
+0x00b2,
+0x00ba,
+0x00bf,
+0x00c4,
+0x00cb,
+0x00ce,
+0x00d5,
+0x00dc,
+0x00e4,
+0x00eb,
+0x00f4,
+0x00f9,
+0x0101,
+0x010a,
+0x0112,
+0x0115,
+0x0118,
+0x011e,
+0x0123,
+0x0128,
+0x012e,
+0x0134,
+0x013c,
+0x0145,
+0x014b,
+0x014e,
+0x0156,
+0x015d,
+0x0162,
+0x016b,
+0x0174,
+0x017b,
+0x017e,
+0x0182,
+0x0189,
+0x018c,
+0x0192,
+0x0198,
+0x019c,
+0x019f,
+0x01a5,
+0x01aa,
+0x01af,
+0x01b3,
+0x01b7,
+0x01bc,
+0x01c3,
+0x01ca,
+0x01d5,
+0x01de,
+0x01e5,
+0x01e8,
+0x01ee,
+0x01f1,
+0x01f6,
+};
+
